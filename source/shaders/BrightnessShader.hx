104c113671aaf58d5c800d9c96d4314abc38f637
someone-121 <133067281+someone-121@users.noreply.github.com>
2023-06-08 16:37:00 -0300

    Bunda

--- source/shaders/BrightnessShader.hx
+++ source/shaders/BrightnessShader.hx
@@ -27,16 +27,16 @@ class BrightnessShaderData extends FlxShader
 	
 	void main()
 	{
-		vec4 color = flixel_texture2D(bitmap, openfl_TextureCoordv);
-		if(color.a == 0)
+		vec4 color = texture2D(bitmap, openfl_TextureCoordv);
+		if(color.a == 0.)
 		{
-			gl_FragColor = vec4(0, 0, 0, 0);
+			gl_FragColor = vec4(0., 0., 0., 0.);
 		}
 		else
 		{
-			float daR = color.r + ((1 - color.r) * brightness);
-			float daG = color.g + ((1 - color.g) * brightness);
-			float daB = color.b + ((1 - color.b) * brightness);
+			float daR = color.r + ((1. - color.r) * brightness);
+			float daG = color.g + ((1. - color.g) * brightness);
+			float daB = color.b + ((1. - color.b) * brightness);
 			
 			gl_FragColor = vec4(daR, daG, daB, color.a);
 		}
